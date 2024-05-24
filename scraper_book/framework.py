4b81ead949efc555ded2d4e33bb13aba4e789517
Denis <dvtolochko@gmail.com>
2024-05-24 12:33:11 -0700

    gpt version
    testing gpt in work, trying to find the issue outsorcing that to chat gpt )

--- scraper_book/framework.py
+++ scraper_book/framework.py
@@ -5,46 +5,45 @@ import datetime
 import random
 
 pages = set()
-random.seed(datetime.datetime.now())
+random.seed(datetime.datetime.now().timestamp())
 
-#extract list of internal links found on the page
+# Extract list of internal links found on the page
 def getInternalLinks(bsObj, includeUrl):
     internalLinks = []
-    #will find all links starts from "/"
+    # Will find all links starting with "/"
     for link in bsObj.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in internalLinks:
                 internalLinks.append(link.attrs['href'])
     return internalLinks
 
-#extracting list of all external links was found on the page
+# Extracting list of all external links found on the page
 def getExternalLinks(bsObj, excludeUrl):
     externalLinks = []
-    #fill find all links start from http or www and do not content current address.
-    for link in bsObj.findAll("a",href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+    # Will find all links starting with http or www and not containing the current address
+    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
         if link.attrs['href'] is not None:
-            if link.attrs['href'] not in  externalLinks:
-                externalLinks.apend(link.attrs['href'])
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
     return externalLinks
 
 def splitAddress(address):
-    addressParts = address.replace("http://","").split("/")
+    addressParts = address.replace("http://", "").split("/")
     return addressParts
 
-def getRandomExternalLinks(startingPage):
+def getRandomExternalLink(startingPage):
     html = urlopen(startingPage)
-    bsObj = BeautifulSoup(html)
+    bsObj = BeautifulSoup(html, 'html.parser')
     externalLinks = getExternalLinks(bsObj, splitAddress(startingPage)[0])
     if len(externalLinks) == 0:
-        internalLinks = getInternalLinks(startingPage)
-        return getNextExternalLink(internalLinks[random.randint(0,len(internalLinks)-1)])
+        internalLinks = getInternalLinks(bsObj, splitAddress(startingPage)[0])
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks)-1)])
     else:
         return externalLinks[random.randint(0, len(externalLinks)-1)]
 
 def followExternalOnly(startingSite):
-    externalLink = getRandomExternalLink("http://oreilly.com")
-    print("Random external link is: "+externalLink)
+    externalLink = getRandomExternalLink(startingSite)
+    print("Random external link is: " + externalLink)
     followExternalOnly(externalLink)
 
 followExternalOnly("http://oreilly.com")
-
